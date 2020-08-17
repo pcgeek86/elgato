bb1cc87b7c00c5917abb25af95921ae38b05e256
Trevor Sullivan <trevor@trevorsullivan.net>
2020-08-17 08:54:48 -0600

    Adding help docs to Set-ElgatoKeyLight function.

--- elgato.psm1
+++ elgato.psm1
@@ -1,4 +1,20 @@
 function Set-ElgatoKeyLight {
+    <#
+    .Synopsis
+    Configures an Elgato Key Light Air that's connected to the local network. Control Center and Stream Deck software not required.
+
+    .Parameter Hostname
+    DNS Hostname or IP address of the target light.
+
+    .Parameter Brightness
+    The brightness of the light that you are configuring. Valid values: 3 to 100.
+
+    .Parameter Temperature
+    The color temperature (7000K to 2900K) to set the light to. Valid values: 143 to 344.
+
+    .Parameter On
+    If not specified, the light will be shut off. If specified, the light will be turned on.
+    #>
     [CmdletBinding()]
     param (
       [Parameter(Mandatory = $true)]
