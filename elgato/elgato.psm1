2e598b324dd8fe12aa171af5507cffa68b6703b3
Jaap Brasser <noreply@jaapbrasser.com>
2021-01-23 00:28:59 +0100

    Added Start-Elgato & Stop 🏮

--- elgato/elgato.psm1
+++ elgato/elgato.psm1
@@ -53,7 +53,7 @@ function Set-ElgatoKeyLight {
 function Get-ElgatoKeyLight {
 <#
     .Synopsis
-    Configures an Elgato Key Light Air that's connected to the local network. Control Center and Stream Deck software not required.
+    Gets information about an Elgato Key Light Air that's connected to the local network. Control Center and Stream Deck software not required.
 
     .Parameter Hostname
     DNS Hostname or IP address of the target light, supports multiple objects
@@ -78,4 +78,84 @@ function Get-ElgatoKeyLight {
         Invoke-RestMethod -Method Get -Uri http://$_`:9123/elgato/lights
     }
   
-  }
+}
+
+function Start-ElgatoKeyLight {
+<#
+    .Synopsis
+    Turns on an Elgato Key Light Air that's connected to the local network. Control Center and Stream Deck software not required.
+
+    .Parameter Hostname
+    DNS Hostname or IP address of the target light, supports multiple objects
+
+    .Example
+    Turn on a key light air
+
+    PS > Start-ElgatoKeyLight -Host 10.0.0.231
+
+    .Example
+    Turn on both key light airs
+
+    PS > Start-ElgatoKeyLight -Host 10.0.0.231, 10.0.0.232
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]] $Hostname
+    )
+
+    $Body = @{
+        NumberOfLights = 1
+        Lights = @(
+            @{
+                On = 1
+            }
+        )
+    } | ConvertTo-Json
+  
+
+    $HostName | ForEach-Object {
+      Invoke-RestMethod -Method Put -Uri http://$_`:9123/elgato/lights -Body $Body
+    }
+    
+}
+
+function Stop-ElgatoKeyLight {
+<#
+    .Synopsis
+    Turns off an Elgato Key Light Air that's connected to the local network. Control Center and Stream Deck software not required.
+
+    .Parameter Hostname
+    DNS Hostname or IP address of the target light, supports multiple objects
+
+    .Example
+    Turn off a key light air
+
+    PS > Stop-ElgatoKeyLight -Host 10.0.0.231
+
+    .Example
+    Turn off both key light airs
+
+    PS > Stop-ElgatoKeyLight -Host 10.0.0.231, 10.0.0.232
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]] $Hostname
+    )
+
+    $Body = @{
+        NumberOfLights = 1
+        Lights = @(
+            @{
+                On = 0
+            }
+        )
+    } | ConvertTo-Json
+  
+
+    $HostName | ForEach-Object {
+      Invoke-RestMethod -Method Put -Uri http://$_`:9123/elgato/lights -Body $Body
+    }
+    
+}
