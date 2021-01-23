69f5aa23e51b845d2f5a04d0a39bcca0e104227d
Jaap Brasser <noreply@jaapbrasser.com>
2021-01-23 02:04:57 +0100

    Updated tests for changes & new functions 🧪🥼

--- elgato/test.ps1
+++ elgato/test.ps1
@@ -1,17 +1,22 @@
 Import-Module -Name $PSScriptRoot/elgato.psd1 -Force
 
-# Run through a sequence
-Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 100 -Temperature 143
-Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 50 -Temperature 143
-Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 75 -Temperature 143
-Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 100 -Temperature 344
-Start-Sleep -Milliseconds 2000
-Set-ElgatoKeyLight -Host 10.0.0.231 -Brightness 50 -Temperature 143
-Start-Sleep -Milliseconds 600
-Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 10 -Temperature 143
-Start-Sleep -Milliseconds 300
-Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 100 -Temperature 143
+# This should get the current settings of both lights
+Get-ElgatoKeyLight -Host 10.0.0.231, 10.0.0.232
 
-# This should switch off both lights, as the -On parameter has not been specified
-Set-ElgatoKeyLight -Host 10.0.0.231
-Set-ElgatoKeyLight -Host 10.0.0.232
+# Run through a sequence
+Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 100 -Temperature 7000
+Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 50 -Temperature 7000
+Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 75 -Temperature 7000
+Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 100 -Temperature 2900
+Start-Sleep -Milliseconds 2000
+Set-ElgatoKeyLight -Host 10.0.0.231 -Brightness 50 -Temperature 7000
+Start-Sleep -Milliseconds 600
+Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 10 -Temperature 7000
+Start-Sleep -Milliseconds 300
+Set-ElgatoKeyLight -On -Host 10.0.0.231 -Brightness 100 -Temperature 7000
+
+# This should switch off both lights
+Stop-ElgatoKeyLight -Host 10.0.0.231, 10.0.0.232
+
+# This should switch on both lights
+Start-ElgatoKeyLight -Host 10.0.0.231, 10.0.0.232
